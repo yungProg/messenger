7b52e5a2622fa12e8b562275d7470df7e4267242
yungProg <22945393+yungProg@users.noreply.github.com>
2026-01-12 15:57:48 +0000

    move create from hangout controller to message controller

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -3,15 +3,4 @@ class HangoutsController < ApplicationController
     @message = Message.new
     @messages = Message.includes(:user)
   end
-  def create
-    @message = current_user.messages.build(message_params)
-    @message.save
-    ActionCable.server.broadcast("message", @message.as_json(include: :user))
-  end
-
-  private
-
-  def message_params
-    params.expect(message: [ :body ])
-  end
 end
